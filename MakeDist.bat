ef4c302823618f5a68028f68e0cd5d2c86062d37
David Kinder <david.kinder@david.kinder>
2015-03-15 19:38:52 +0000

    Fix batch file on x64 systems

--- MakeDist.bat
+++ MakeDist.bat
@@ -1,22 +1,22 @@
 @echo off
 
-"\Program Files\Zip\zip" -j \Temp\WindowsFrotz.zip Win\Release\Frotz.exe Win\Release\Frotz.chm
-"\Program Files\Zip\zip" -j \Temp\WindowsFrotz.zip Win\Release\*.dll
-"\Program Files\Zip\zip" \Temp\WindowsFrotz.zip COPYING Examples\*
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WindowsFrotz.zip Win\Release\Frotz.exe Win\Release\Frotz.chm
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WindowsFrotz.zip Win\Release\*.dll
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotz.zip COPYING Examples\*
 
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip COPYING Blorb\*.c Blorb\*.h Generic\*.c Generic\*.h
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Win\* Win\res\* Win\help\* MakeDist.bat Installer\*
-"\Program Files\Zip\zip" -r \Temp\WindowsFrotzSrc.zip Win\Frotz*\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip COPYING Blorb\*.c Blorb\*.h Generic\*.c Generic\*.h
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Win\* Win\res\* Win\help\* MakeDist.bat Installer\*
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\WindowsFrotzSrc.zip Win\Frotz*\*
 pushd \Programs
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\libmodplug\*
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\mfc\*
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.h
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.cpp
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.def
-"\Program Files\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\Makefile
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\libmodplug\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\mfc\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.h
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.cpp
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\*.def
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsFrotzSrc.zip Libraries\ScaleGfx\Makefile
 popd
 
 pushd Installer
-"\Program Files\NSIS\makensis" WindowsFrotz.nsi
+"%ProgramFiles(x86)%\NSIS\makensis" WindowsFrotz.nsi
 move *.exe \Temp
 popd
