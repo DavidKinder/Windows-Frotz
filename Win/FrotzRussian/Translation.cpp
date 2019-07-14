8a0db69bb7fa7add55ffbf021341c98fdfa3b24c
David Kinder <david.kinder@david.kinder>
2019-07-14 20:05:53 +0100

    Add links to Help menu specific to the Russian translation

--- Win/FrotzRussian/Translation.cpp
+++ Win/FrotzRussian/Translation.cpp
@@ -14,3 +14,24 @@ extern "C" __declspec(dllexport) BOOL IsEnabled(VOID)
 {
   return (GetACP() == 1251); // ANSI Cyrillic; Cyrillic (Windows)
 }
+
+extern "C" __declspec(dllexport) LPCSTR GetLink(UINT idx)
+{
+  switch (idx)
+  {
+  case 0:
+    return "https://ifiction.ru";
+  case 1:
+    return "https://ifwiki.ru";
+  case 2:
+    return "https://ifhub.club";
+  case 3:
+    return "https://parserfest.ru";
+  case 4:
+    return "https://kril.ifiction.ru";
+  case 5:
+    return "https://rinform.org";
+  default:
+    return NULL;
+  }
+}
